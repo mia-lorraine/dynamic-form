6e29fe140580401e97dbc0e1dea51f65ba112f4b
Mia Laurea <mlaurea@llu.edu>
2024-07-25 14:43:19 -0700

    testing

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,27 +2,81 @@
 import jsonData from '../form/form.json'
 export default {
   mounted() {
-    // console.log(jsonData.map((q) => q?.question_dependencies))
-    console.log(jsonData.filter((q) => !q?.question_dependencies))
-    console.log(jsonData.filter((q) => q?.question_dependencies))
+    // format the data and add a new object titled: dependsOn
+    jsonData.forEach((question) => {
+      this.formattedQuestions[question.question_id] = question
+      this.answers[question.question_id] = question.answer
+      // Key: Q1 Value {{all values}}
+    })
+    console.log('answers', this.answers)
+
+    // jsonData.forEach((question) => {
+    //   if (question?.question_dependencies) {
+    //     Object.keys(question.question_dependencies).forEach((key) => {
+    //       if (question.question_dependencies[key] !== '') {
+    //         if (question.question_id.answer) {
+    //           this.formattedQuestions[question.question_id] = { ...question, show: true }
+    //           console.log('what is the new formatted question', this.formattedQuestions)
+    //         }
+    //         console.log('yuh', this.answers[question.question_id] == `${key}`)
+    //         if (this.answers[question.question_id] === key) {
+    //           console.log(
+    //             ' if',
+    //             question.question_id,
+    //             ' answer is',
+    //             key,
+    //             'show ',
+    //             question.question_dependencies[key]
+    //           )
+    //         }
+    //         if (this.formattedQuestions[question.question_id].answer === key) {
+    //           console.log('show', question.question_dependencies[key])
+    //         }
+    //       }
+    //     })
+    //   }
+    // })
+
+    jsonData.forEach((question) => {
+      if (question?.question_dependencies) {
+        Object.keys(question?.question_dependencies).forEach((key) => {
+          if (question.question_dependencies[key] !== '') {
+            // console.log('question', question.question_dependencies)
+            this.dependencies[question.question_dependencies[key]] = question
+
+            this.newQuestions.push({
+              question_id: question.question_id,
+              question: question.question_details,
+              dependsOn: {
+                answer: key,
+                questionId: question.question_dependencies[key]
+              }
+            })
+          }
+        })
+      } else {
+        this.newQuestions.push({
+          question_id: question.question_id,
+          question: question.question_details,
+          type: question.type,
+          answer: question.answer
+          // show: true
+        })
+      }
+    })
+    // console.log('depedencies', this.dependencies)
+    // console.log('new questions', this.newQuestions)
   },
   data() {
     return {
+      newQuestions: [],
+      dependencies: [],
+      formattedQuestions: {},
+      answers: {},
       errors: [],
       pageNumber: 0,
       size: 4,
-      formItems: jsonData,
-      form_answers: {
-        Q: []
-      },
-      conditions: [
-        { question: 'Q4', value: 'Yes', render: 'Q7' },
-        { question: 'Q8', value: 'Yes', render: 'Q9' },
-        { question: 'Q8', value: 'No', render: 'Q11' },
-        { question: 'Q9', value: 'Yes', render: 'Q10' },
-        { question: 'Q11', value: 'Yes', render: 'Q10' },
-        { question: 'Q11', value: 'no', render: 'Q12' }
-      ]
+      formItems: jsonData
     }
   },
   methods: {
@@ -36,14 +90,9 @@ export default {
       console.log('what is the form data...', formData)
       this.validateData(formData)
     },
-    checkForDependencies(dependencies, answer, i) {
-      console.log(dependencies.yes, i, answer, dependencies.no)
-    },
-    validateData(data) {
-      console.log('validating data', data.Q.length)
-      for (let i = 0; i <= data.length - 1; i++) {
-        if (data[i] === '') this.errors.push('Please fill out field')
-      }
+    validateData(data) {},
+    updateAnswer(answer, q) {
+      this.answer[q]
     }
   },
   computed: {
@@ -55,43 +104,41 @@ export default {
     paginatedData() {
       const start = this.pageNumber * this.size,
         end = start + this.size
-      return this.formItems.slice(start, end)
+      return this.newQuestions.slice(start, end)
     },
-    dependencies() {
-      for (let i = 0; i <= this.formItems.length - 1; i++) {
-        console.log('computing...', this.formItems[i])
-        if (!this.formItems[i].question_dependencies) return false
-        else if (this.formItems[i].question_dependencies) {
-          console.log('computiong...', this.formItems[i])
-          return true
+    computeShow() {
+      return this.paginatedData.forEach((question) => {
+        console.log('comping..', this.answers[question.question_id] == question?.dependsOn?.answer)
+        if (this.answers[question.question_id] == question?.dependsOn?.answer) {
+          this.paginatedData[question.question_id] = { ...question, show: true }
         } else {
           return false
         }
-      }
-      return true
+      })
     }
   }
 }
 </script>
 
 <template>
-  <!-- //can make component -->
+  <form @submit.prevent="submitForm(this.answers)">
+    <div class="form-group" v-for="(question, key, index) in paginatedData" v-bind:key="index">
+      <label
+        >{{ question.question_id }}. {{ question?.dependsOn?.questionId }}
+        {{ this.answers[question.question_id] }}
+        <!-- {{ question }} -->
+      </label>
 
-  <form @submit.prevent="submitForm(form_answers)">
-    <div class="form-group" v-for="(question, index) in paginatedData" v-bind:key="index">
-      <label>{{ question.question_id }} {{ question.question_details }} </label>
-      <input
-        v-if="!question.question_dependencies"
-        v-model="form_answers.Q[index]"
+      <!-- <input
+        v-if="!question.dependsOn"
+        v-model="this.answers[quesiton?.question_id]"
         class="form-control"
-        :type="question.type"
-      />
+      /> -->
       <input
-        v-if="question.question_dependencies"
-        @change="checkForDependencies(question.question_dependencies, form_answers.Q[index], index)"
-        v-model="form_answers.Q[index]"
+        v-if="question.show"
+        @change="updateAnswer(this.answers[question.question_id], question.quesiton_id)"
+        v-model="this.answers[question.question_id]"
         class="form-control"
-        :type="question.type"
       />
       <!-- <input
         v-if="dependencies"
